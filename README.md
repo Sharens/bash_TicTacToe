666b8a3b68695060359cfa64c8ea0456b004a14c
Sharens <xarens19@gmail.com>
2024-11-01 15:47:57 +0100

    Dodanie funkcjonalności gry turowej; Aktualizacja README

--- README.md
+++ README.md
@@ -1,6 +1,56 @@
-# bash_TicTacToe
+# Kółko i krzyżyk w bashu
 
-Backlog:
-[] - działa w trybie gry turowej,
-[]- pozwala na zapis i odtwarzanie przerwanej gry (save game),
-[] - pozwala na grę z komputerem.
+## Backlog:
+[x] - działa w trybie gry turowej,
+[ ] - pozwala na zapis i odtwarzanie przerwanej gry (save game),
+[ ] - pozwala na grę z komputerem.
+[ ] - dodać kontrolę nad inputem użytkownika.
+
+Ten projekt to implementacja gry w kółko i krzyżyk (Tic-Tac-Toe) w skrypcie bash. Gra pozwala dwóm graczom na rywalizację na planszy o wymiarach 3x3, gdzie gracze na zmianę wybierają pola i dążą do uzyskania trzech swoich symboli w linii.
+
+## Zawartość projektu
+
+- **main.sh**: główny skrypt zawierający logikę gry.
+
+## Uruchomienie
+
+1. Skrypt można uruchomić bezpośrednio w terminalu. Upewnij się, że masz uprawnienia do uruchamiania pliku, w razie potrzeby nadaj je:
+
+    ```bash
+    chmod +x main.sh
+    ```
+
+2. Uruchom grę poleceniem:
+
+    ```bash
+    ./main.sh
+    ```
+
+## Zasady gry
+
+1. Po uruchomieniu skryptu wyświetla się ekran powitalny.
+2. Gracz wybiera, czy chce grać symbolem "X" lub "O".
+3. Gra wyświetla planszę, a gracze na zmianę wybierają pole, na którym chcą umieścić swój symbol (wpisując liczbę od 1 do 9).
+4. Wygrywa pierwszy gracz, który uzyska trzy swoje symbole w linii – poziomo, pionowo lub na ukos.
+
+## Struktura kodu
+
+### Zmienne
+- `player_1` i `player_2`: symbol pierwszego i drugiego gracza.
+- `turn`: licznik tur.
+- `game_on`: wskaźnik, czy gra trwa.
+- `field`: tablica przechowująca stan planszy.
+
+### Funkcje
+
+- `print_board`: wyświetla aktualny stan planszy.
+- `welcome_screen`: wyświetla ekran powitalny.
+- `starting_player`: pozwala na wybór gracza rozpoczynającego.
+- `select_field`: pozwala graczowi wybrać pole i aktualizuje stan planszy.
+- `change_player`: przełącza tury pomiędzy graczami.
+- `check_win`: sprawdza, czy na planszy uzyskano trzy symbole w linii i wyświetla zwycięzcę.
+- `choose_winner`: sprawdza zwycięskie kombinacje na planszy.
+
+## Wymagania
+
+- Interpreter `bash`
--- main.sh
+++ main.sh
@@ -11,6 +11,7 @@ game_on=true
 field=( 1 2 3 4 5 6 7 8 9 )
 
 print_board(){
+    echo -e "\n"
     echo " ${field[0]}   |   ${field[1]}   |   ${field[2]} "
     echo "--------------------"
     echo " ${field[3]}   |   ${field[4]}   |   ${field[5]} "
@@ -21,31 +22,101 @@ print_board(){
 welcome_screen(){
     clear
     echo "### START GRY ###"
-    echo "Poniżej znajdziesz plansze: "
-
 }
 
 starting_player() {
+    echo -e "\n"
     echo "Który gracz ma zaczynać grę: (X/O) "
-    read current_player
+
+    while true; do
+        read current_player
+        if [ "$current_player" == "X" ] || [ "$current_player" == "O" ]; then
+            break
+        else
+            echo "Niepoprawnie wybrano gracza. Wybierz 'X' lub 'O'."
+        fi
+    done
+
     echo "Grę rozpoczyna gracz: "$current_player
 }
 
-welcome_screen
-
-print_board
-
-starting_player
-
 select_field () {
+    echo -e "\n"
     echo "Wybierz pole od 1 - 9: " # Dodać sprawdzanie czy gracz wybrał poprawne pole
     read input_field
     select_field=${field[($input_field-1)]}
-    (($turn+=1))
+    ((turn+=1))
 
     field[$input_field-1]=$current_player
 }
 
+change_player () {
+    if [ "$current_player" == "X" ]; then
+        current_player="O"
+    else
+        current_player="X"
+    fi
+        echo -e "\n"
+    echo "### Teraz tura gracza " $current_player
+}
+
+check_win () {
+    if [[ ${field[$1]} == ${field[$2]} && ${field[$2]} == ${field[$3]} ]]; then
+        game_on=false
+    fi
+    if [ $game_on == false ]; then
+        if [ "${field[$1]}" == 'X' ]; then
+            echo -e "\n"
+            echo "################"
+            echo "Wygrał gracz: X"
+            echo "################"
+            return
+        else
+            echo -e "\n"
+            echo "################"
+            echo "Wygrał gracz: O"
+            echo "################"
+            return
+        fi
+    fi
+}
+
+choose_winner () {
+    if [ $game_on == false ]; then return; fi
+    check_win 0 1 2
+
+    if [ $game_on == false ]; then return; fi
+    check_win 3 4 5
+
+    if [ $game_on == false ]; then return; fi
+    check_win 6 7 8
+
+    if [ $game_on == false ]; then return; fi
+    check_win 0 3 6
+
+    if [ $game_on == false ]; then return; fi
+    check_win 1 4 7
+
+    if [ $game_on == false ]; then return; fi
+    check_win 2 5 8
+
+    if [ $game_on == false ]; then return; fi
+    check_win 0 4 8
+
+    if [ $game_on == false ]; then return; fi
+    check_win 2 4 6
+}
+
+welcome_screen
+starting_player
+print_board
 select_field
 print_board
 
+while $game_on
+do
+    change_player
+    select_field
+    print_board
+    choose_winner
+done
